8c02c0e02bc0f2111a237109836643ff10a59ec3
Bhagya Lekshmi B <bhagyalekshmib3@gmail.com>
2026-09-05 11:41:48 +0530

    Time: 0 ms (100%), Space: 20.3 MB (64.12%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        freq={}
+        for i in range(len(nums)):
+            comp=target-nums[i]
+            if comp in freq:
+                return [i,freq[comp]]
+            freq[nums[i]]=i
+            
